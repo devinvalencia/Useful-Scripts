16c8c6603f1779345a8012ff6b892589c479d402
Devin Valencia <Devinvalencia@yahoo.com>
2021-01-07 18:44:11 -0500

    first commit

---
+++ mail_script_portal_link.js
@@ -0,0 +1,26 @@
+// Type: Mail Script
+// Obj: Creates button to portal where current record is opened in widget
+
+(function runMailScript( /* GlideRecord */ current, /* TemplatePrinter */ template,
+    /* Optional EmailOutbound */
+    email, /* Optional GlideRecord */ email_action,
+    /* Optional GlideRecord */
+    event) {
+
+	var backgroundColor = 'background-color: #278efc;';
+	var border = 'border: 1px solid #0368d4;';
+	var color = 'color: #ffffff;';
+	var fontSize = 'font-size: 16px;';
+	var fontFamily = 'font-family: Helvetica, Arial, sans-serif;';
+	var textDecoration = 'text-decoration: none; border-radius: 3px;';
+	var webKitBorder = '-webkit-border-radius: 3px;';
+	var mozBorder = '-moz-border-radius: 3px;';
+	var display = 'display: inline-block;';
+	var padding = 'padding: 5px;';
+
+    var url = '<a href="' + gs.getProperty('glide.servlet.uri') + 'esc?id=ticket&table=' + current.sys_class_name + '&sys_id=' + current.sys_id + '" style="' + backgroundColor + border + color + fontSize + fontFamily + textDecoration + webKitBorder + mozBorder + display + padding +'">Spot portal</a>';
+    // gs (GlideSystem) function gets instance url, class name gets table
+
+    template.print(url);
+
+})(current, template, email, email_action, event);
